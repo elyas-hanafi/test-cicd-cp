fe31f94f7eeccc4b3016235bfe1f5eda079a436a
elyas_hanafi <eliinsta79@gmail.com>
2024-01-24 23:30:06 +0330

    test with another host

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         <Link href={`#section1`} className="text-white">goo</Link>
         <Link href={`#section2`} className="text-white">goo</Link>
       </div>
-      <div id="section1" className="h-screen bg-black">Heeeysss</div>
+      <div id="section1" className="h-screen bg-black">Heeeysssss</div>
       <div id="section2" className="h-screen bg-red-900">Heeeysss</div>
     </>
   );
