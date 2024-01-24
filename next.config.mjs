5a168f060a26478e1a81ccce99a7ba51897c52f7
elyas_hanafi <eliinsta79@gmail.com>
2024-01-24 22:26:45 +0330

    create work flow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'export',
+  distDir: 'build'
+};
 
-export default nextConfig;
+module.exports = nextConfig;
